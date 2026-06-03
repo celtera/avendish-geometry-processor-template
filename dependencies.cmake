974a9935f437c8e57dd784992ad9c21291d1ea8b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-06-03 11:34:39 -0400

    update avendish

--- dependencies.cmake
+++ dependencies.cmake
@@ -3,11 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   avendish
   GIT_REPOSITORY "https://github.com/celtera/avendish"
-  # Geometry back-ends (TouchDesigner SOP/POP, Godot GEOMETRY, Max/MSP ob3d)
-  # require a recent Avendish: avnd_make_geometry() and the Max geometry (ob3d)
-  # binding were introduced after the audio template's pinned commit, so we track
-  # the feature/max-obj3d revision here.
-  GIT_TAG  b318737b92dcce8f0df08949759cd1bddf72c5fa
+  GIT_TAG  f26e07e04f88261fb96b6538f9c1e7b7940b473c
   GIT_PROGRESS true
 )
 FetchContent_Populate(avendish)
